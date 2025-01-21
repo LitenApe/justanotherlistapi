b69dd02e8ba7e7213fb7c63c3cbf633508787ea0
Son Thanh Vo <sonvo95@outlook.com>
2025-01-21 09:58:04 +0100

    count completed items

--- Checklist/Controller/ItemGroupController.cs
+++ Checklist/Controller/ItemGroupController.cs
@@ -11,15 +11,15 @@ namespace JustAnotherListAPI.Checklist.Controller
         {
             Guid userId = Guid.Parse("ed1e87c8-4823-4364-b3ee-4d9f13a07300");
 
-            var membersDb = db.Members;
-            var itemGroupDb = db.ItemGroups;
+            var memberDb = db.Members;
+            var itemGroupDb = db.ItemGroups.Include(ig => ig.Items);
 
-            var lists = await membersDb.Where(m => m.MemberId == userId)
-              .GroupJoin(
+            var lists = await memberDb.Where(m => m.MemberId == userId)
+              .Join(
                 itemGroupDb,
                 ig => ig.ItemGroupId,
                 m => m.Id,
-                (m, ig) => ig
+                (m, ig) => ItemGroupDTO.Create(ig)
               )
               .ToListAsync();
 
--- Checklist/Model/ItemGroupDTO.cs
+++ Checklist/Model/ItemGroupDTO.cs
@@ -4,14 +4,20 @@ namespace JustAnotherListAPI.Checklist.Model
 {
     public class ItemGroupDTO
     {
+        public Guid Id { get; set; }
         [Required]
         public required string Name { get; set; }
+        public int Complete { get; set; } = 0;
+        public int Incomplete { get; set; } = 0;
 
         public static ItemGroupDTO Create(ItemGroup itemGroup)
         {
             return new()
             {
-                Name = itemGroup.Name
+                Id = itemGroup.Id,
+                Name = itemGroup.Name,
+                Complete = itemGroup.Items == null ? 0 : itemGroup.Items.Where(i => i.IsComplete).Count(),
+                Incomplete = itemGroup.Items == null ? 0 : itemGroup.Items.Where(i => i.IsComplete == false).Count()
             };
         }
     }
