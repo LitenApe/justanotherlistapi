5650f87f94dd6793ec7ac4ffac84ea21c4a78c60
Son Thanh Vo <sonvo95@outlook.com>
2025-04-20 13:00:32 +0200

    refactor: use predefined constant for bearer scheme

--- Core/Program.cs
+++ Core/Program.cs
@@ -2,84 +2,87 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
-namespace Core
+namespace Core;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services
+
+        // Database
+        builder.Services.AddDbContext<DatabaseContext>(opt =>
         {
-            var builder = WebApplication.CreateBuilder(args);
+            opt.UseInMemoryDatabase("JustAnotherList");
+        });
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            // Add services
+        // Auth
+        builder.Services.AddAuthentication(IdentityConstants.BearerScheme);
+        builder.Services.AddAuthorization();
+        builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+            .AddEntityFrameworkStores<DatabaseContext>();
 
-            // Database
-            builder.Services.AddDbContext<DatabaseContext>(opt =>
+        // API Documentation
+        builder.Services.AddOpenApi();
+
+        // Configure services
+
+        // Auth
+        builder.Services.Configure<IdentityOptions>(opt =>
+        {
+            opt.Password.RequireDigit = true;
+            opt.Password.RequireLowercase = true;
+            opt.Password.RequireNonAlphanumeric = true;
+            opt.Password.RequireUppercase = true;
+            opt.Password.RequiredLength = 6;
+            opt.Password.RequiredUniqueChars = 1;
+
+            opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3);
+            opt.Lockout.MaxFailedAccessAttempts = 7;
+            opt.Lockout.AllowedForNewUsers = true;
+
+            opt.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            opt.User.RequireUniqueEmail = false;
+        });
+        builder.Services.ConfigureApplicationCookie(opt =>
+        {
+            // Cookie settings
+            opt.Cookie.HttpOnly = true;
+            opt.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+            opt.LoginPath = "/auth/login";
+            opt.AccessDeniedPath = "/auth/denied";
+            opt.SlidingExpiration = true;
+        });
+
+        var app = builder.Build();
+
+        app.UseHttpsRedirection();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapIdentityApi<IdentityUser>();
+        app.MapChecklistApi();
+
+        app.MapOpenApi();
+        app.MapScalarApiReference(opt =>
+        {
+            opt.Authentication = new ScalarAuthenticationOptions
             {
-                opt.UseInMemoryDatabase("JustAnotherList");
-            });
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+                PreferredSecurityScheme = IdentityConstants.BearerScheme,
+            };
+            opt
+                .WithPreferredScheme(IdentityConstants.BearerScheme)
+                .WithHttpBearerAuthentication(bearer =>
+                {
+                    bearer.Token = "your-bearer-token";
+                });
+        });
 
-            // Auth
-            builder.Services.AddAuthentication("Bearer");
-            builder.Services.AddAuthorization();
-            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
-                .AddEntityFrameworkStores<DatabaseContext>();
-
-            // API Documentation
-            builder.Services.AddOpenApi();
-
-            // Configure services
-
-            // Auth
-            builder.Services.Configure<IdentityOptions>(opt =>
-            {
-                opt.Password.RequireDigit = true;
-                opt.Password.RequireLowercase = true;
-                opt.Password.RequireNonAlphanumeric = true;
-                opt.Password.RequireUppercase = true;
-                opt.Password.RequiredLength = 6;
-                opt.Password.RequiredUniqueChars = 1;
-
-                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(3);
-                opt.Lockout.MaxFailedAccessAttempts = 7;
-                opt.Lockout.AllowedForNewUsers = true;
-
-                opt.User.AllowedUserNameCharacters =
-                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-                opt.User.RequireUniqueEmail = false;
-            });
-            builder.Services.ConfigureApplicationCookie(opt =>
-            {
-                // Cookie settings
-                opt.Cookie.HttpOnly = true;
-                opt.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
-                opt.LoginPath = "/auth/login";
-                opt.AccessDeniedPath = "/auth/denied";
-                opt.SlidingExpiration = true;
-            });
-
-            var app = builder.Build();
-
-            app.UseHttpsRedirection();
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.MapIdentityApi<IdentityUser>();
-            app.MapChecklistApi();
-
-            app.MapOpenApi();
-            app.MapScalarApiReference(opt =>
-            {
-                opt
-                    .WithPreferredScheme("Bearer")
-                    .WithHttpBearerAuthentication(bearer =>
-                    {
-                        bearer.Token = "your-bearer-token";
-                    });
-            });
-
-            app.Run();
-        }
+        app.Run();
     }
 }
