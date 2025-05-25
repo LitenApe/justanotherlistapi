d54ab36dd7ec8295ae01cf2cb194a6fa2f514c83
Son Thanh Vo <sonvo95@outlook.com>
2025-05-25 18:44:52 +0200

    test: add test for adding member on created group

--- Core.Tests/Checklist/ItemGroup/CreateItemGroupTests.cs
+++ Core.Tests/Checklist/ItemGroup/CreateItemGroupTests.cs
@@ -94,7 +94,56 @@ public class CreateItemGroupTests
             }
             else
             {
-                Assert.Fail("Expected a database entry with returned guid");
+                Assert.Fail("Expected a Created<ItemGroup> result.");
+            }
+        }
+        else
+        {
+            Assert.Fail("Expected a Results<Created<ItemGroup>, BadRequest, UnauthorizedHttpResult> result.");
+        }
+    }
+
+    [Fact]
+    public async Task Execute_AddUserAsMemberOfItemGroup_WhenItemGroupIsCreated()
+    {
+        // Arrange
+        var request = new CreateItemGroup.Request { Name = "Test Group" };
+        var userId = Guid.NewGuid();
+
+        // Mock ClaimsPrincipal
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, userId.ToString())
+        };
+        var identity = new ClaimsIdentity(claims, "TestAuthType");
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+
+        // Mock DatabaseContext
+        var options = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+        await using var dbContext = new DatabaseContext(options);
+
+        // Act
+        var result = await CreateItemGroup.Execute(request, claimsPrincipal, dbContext);
+
+        // Assert
+        Assert.NotNull(result);
+        if (result is Results<Created<ItemGroup>, BadRequest, UnauthorizedHttpResult> results)
+        {
+            if (results.Result is Created<ItemGroup> createdResult)
+            {
+                var itemGroup = createdResult.Value;
+                Assert.NotNull(itemGroup);
+
+                var dataEntry = await dbContext.Members
+                    .FirstOrDefaultAsync(m => m.MemberId == userId.ToString() && m.ItemGroupId == itemGroup.Id);
+
+                Assert.NotNull(dataEntry);
+            }
+            else
+            {
+                Assert.Fail("Expected a Created<ItemGroup> result.");
             }
         }
         else
